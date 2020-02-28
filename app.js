bd1df4aa13171d77068cd0f790df75f7417ee495
austinseaman <apseaman0@gmail.com>
2020-02-27 20:48:51 -0700

    sort array and find missing number

--- app.js
+++ app.js
@@ -32,5 +32,22 @@ const mappedJsx = jsx.mapV2(str => (`<div>${str}</div>`))
 
 // If using React, returns a <div> for each string.
 console.log(mappedJsx)
-//-----------------------------------------------------------
+//---------------------------------------------------------------------------------
 
+// 2. Write a function that takes an unsorted array as a parameter, and returns the missing number.
+
+function findMissingNo(arr){
+    arr.sort(function(a, b){return a - b})
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i+1] - arr[i] !== 1){ // 1 2 3 4 5
+           return arr[i] + 1
+        }
+    }
+ }
+ console.log(findMissingNo([3, 5, 4, 8, 1, 2, 7])) //6
+ console.log(findMissingNo([10, 14, 12, 11, 16, 18, 17, 13])) //15
+//  Guaranteed Conditions:
+//    - The array has at least 3 numbers
+//    - There are no repeated numbers
+// -----------------------------------------------------------------------------------
