aaf5ca965b060d7caf7bec70819adea86a1eaf8e
austinseaman <apseaman0@gmail.com>
2021-03-11 17:21:10 -0700

    laying foundations for react dj app

---
+++ prax/dj-react/src/Mixer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Mixer extends Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            colors: ['white', 'white', 'white', 'white']
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ prax/dj-react/src/Square.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+const Square = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
